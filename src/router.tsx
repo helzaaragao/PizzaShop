fe4f78cb55e0819bae7b074590fa21bf0e498057
Helza Aragão <helza.batista.aragao@gmail.com>
2025-01-31 17:09:35 -0300

    rascunho: setup do projeto

---
+++ src/router.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import { DashBoard } from "./pages/app/dashboard";
+import { SignIn } from "./pages/auth/sign-in";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <AppLayout></AppLayout>,
+        children: [{path: '/', element: <DashBoard></DashBoard>}]
+    },
+    {
+        path:'/',
+        element: <AuthLayout></AuthLayout>, 
+        children: [{path: '/sign-in', element: <SignIn></SignIn>}]
+    }
+])
