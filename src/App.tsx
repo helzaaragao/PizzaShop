89176bf6cb39b63122195a39f557381dc9c61798
Helza Aragão <helza.batista.aragao@gmail.com>
2025-02-06 16:55:35 -0300

    layout do app com cabeçalho e themetoggle

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import {Helmet, HelmetProvider} from 'react-helmet-async'
 import { Toaster } from '@/components/ui/sonner'
+import { ThemeProvider } from './components/theme/theme-provider';
 
 
 export function App() {
 
   return (
     <HelmetProvider>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark" >
       <Helmet titleTemplate="%s | pizza.shop"></Helmet>
          <RouterProvider router={router}/>
          <Toaster richColors/>
+         </ThemeProvider>
     </HelmetProvider>
 
     // quando quer fazer forms e validar = hook-form e zod
--- src/router.tsx
+++ src/router.tsx
@@ -4,12 +4,15 @@ import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { SignUp } from "./pages/auth/sign-up";
+import { Orders } from "./pages/app/orders/orders";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <AppLayout></AppLayout>,
-        children: [{path: '/', element: <DashBoard></DashBoard>}]
+        children: [{path: '/', element: <DashBoard></DashBoard>},
+            {path: '/orders', element: <Orders></Orders>}
+        ]
     },
     {
         path:'/',
