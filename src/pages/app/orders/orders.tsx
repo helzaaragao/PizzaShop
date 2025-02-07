2d3783b739179ef8d8f7e2a9c6cd23910683572f
Helza Aragão <helza.batista.aragao@gmail.com>
2025-02-07 16:02:17 -0300

    order table row e filter

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,18 +1,38 @@
-import { Input } from "@/components/ui/input";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Helmet } from "react-helmet-async";
+import { OrderTableRow } from "./order-table-row";
+import { OrderTableFilters } from "./order-table-filter";
 
 export function Orders(){ 
     return(
         <>
             <Helmet title="Pedidos"></Helmet>
             <div className="flex flex-col gap-4">
-                <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+                <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>  
             </div>
             <div className="space-y-2.5">
-                <form className="flex items-center gap-2">
-                    <span className="text-sm font-semibold">Filtros</span>
-                    <Input placeholder="Nome do cliente"></Input>
-                </form>
+                    <OrderTableFilters></OrderTableFilters>
+                <div className="border rounded-md">
+                    <Table>
+                    <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[64px]"></TableHead>
+                                <TableHead className="w-[140px]">Identificador</TableHead>
+                                <TableHead className="w-[180px]">Realizado há</TableHead>
+                                <TableHead className="w-[140px]">Status</TableHead>
+                                <TableHead>Cliente</TableHead>
+                                <TableHead className="w-[140px]">Total do pedido</TableHead>
+                                <TableHead className="w-[164px]"></TableHead>
+                                <TableHead className="w-[132px]"></TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                        {Array.from({ length: 10 }).map((_, i) => {
+                            return <OrderTableRow key={i} />
+                        })}
+                        </TableBody>
+                    </Table>
+                </div>
             </div>
         </>
     )
