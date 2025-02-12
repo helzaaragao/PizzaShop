bd1920c476c60ee10daa2548deb1c3ebd989ca15
Helza Aragão <helza.batista.aragao@gmail.com>
2025-02-12 17:19:23 -0300

    dashboard página

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,28 @@
+import { Helmet } from "react-helmet-async";
+import { MonthRevenueCard } from "./month-revenue-card";
+import { MonthOrdersAmountCard } from "./month-orders-amount-card";
+import { DayOrdersAmountCard } from "./day-orders-amount-card";
+import { MonthCanceledOrdersAmountCard } from "./month-canceled-orders-amount-card";
+import { RevenueChart } from "./revenue-chart";
+
+export function DashBoard(){
+    return (
+      <>
+    <Helmet title="Dashboard"></Helmet>
+    <div className="flex flex-col gap-4">
+        <h1 className="text-3xl tracking-tighter font-bold">Dashboard</h1>
+        <div className="grid grid-cols-4 gap-4">
+        <MonthRevenueCard></MonthRevenueCard>
+         <MonthOrdersAmountCard></MonthOrdersAmountCard>
+         <DayOrdersAmountCard></DayOrdersAmountCard>
+         <MonthCanceledOrdersAmountCard></MonthCanceledOrdersAmountCard>
+        </div>
+        <div className="grid grid-cols-9 gap-4">
+            <RevenueChart></RevenueChart>
+        </div>
+    </div>
+    
+   
+      </>
+    )
+}
--- src/pages/app/orders/order-details.tsx
+++ src/pages/app/orders/order-details.tsx
@@ -68,13 +68,13 @@ export function OrderDetails(){
                             <TableCell className="text-right">R$ 59,98</TableCell>
                             <TableCell className="text-right">R$ 109,88</TableCell>
                         </TableRow>
+                        </TableBody>
                         <TableFooter>
                             <TableRow>
                                 <TableCell colSpan={3}>Total do pedido</TableCell>
                                 <TableCell className="text-right font-medium">R$ 260,68</TableCell>
                             </TableRow>
                         </TableFooter>
-                    </TableBody>
                 </Table>
             </div>
         </DialogContent>
