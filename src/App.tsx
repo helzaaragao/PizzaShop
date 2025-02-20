b9d55cca7ee56e0b39dfaa47a88fcea7eb9571e2
Helza Aragão <helza.batista.aragao@gmail.com>
2025-02-20 16:00:33 -0300

    começando a conectar o backend com o frontend: axios

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { router } from './router';
 import {Helmet, HelmetProvider} from 'react-helmet-async'
 import { Toaster } from '@/components/ui/sonner'
 import { ThemeProvider } from './components/theme/theme-provider';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './lib/react-query';
 
 
 export function App() {
@@ -12,7 +14,9 @@ export function App() {
     <HelmetProvider>
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark" >
       <Helmet titleTemplate="%s | pizza.shop"></Helmet>
+         <QueryClientProvider client={queryClient}>
          <RouterProvider router={router}/>
+         </QueryClientProvider>
          <Toaster richColors/>
          </ThemeProvider>
     </HelmetProvider>
