cf4c9220a931ce6c8d78f26f41d5bdde36d3ebc2
Helza Aragão <helza.batista.aragao@gmail.com>
2025-02-27 15:34:34 -0300

    modal de atualização + loading menu com delay

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './global.css'; 
+import '../global.css'; 
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import {Helmet, HelmetProvider} from 'react-helmet-async'
@@ -22,6 +22,7 @@ export function App() {
     </HelmetProvider>
 
     // quando quer fazer forms e validar = hook-form e zod
+   
   
   )
 }
--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -4,42 +4,61 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel,
 import { useQuery } from "@tanstack/react-query";
 import { getProfile } from "@/api/get-profile";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
+import { Skeleton } from "./ui/skeleton";
+import { Dialog } from "@radix-ui/react-dialog";
+import { DialogTrigger } from "./ui/dialog";
+import { StoreProfileDialog } from "./store-profile-dialog";
 
 export function AccountMenu(){  
-    const {data: profile} = useQuery({
+    const {data: profile, isLoading: isLoadingProfile} = useQuery({
         queryKey: ['profile'], 
         queryFn: getProfile,
     })
 
-    const {data: managedRestaurant} = useQuery({
+    const {data: managedRestaurant, isLoading: isLoadingManagedRestaurant} = useQuery({
         queryKey: ['managed-restaurant'], 
         queryFn: getManagedRestaurant,
     })
 
     return(
+        <Dialog>
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline"
                   className="flex select-none items-center gap-2"
-                >{managedRestaurant?.name}
+                >{isLoadingManagedRestaurant ? ( <Skeleton className="h-4 w-40"></Skeleton>
+                ) : managedRestaurant?.name}
                     <ChevronDown className="h-4 w-4"></ChevronDown>
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className="w-56">
                 <DropdownMenuLabel className="flex flex-col">
-                    <span>{profile?.name}</span>
-                     <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
+                    {isLoadingProfile ? (
+                        <div className="space-y-1.5">
+                            <Skeleton className="h-4 w-32"></Skeleton>
+                            <Skeleton className="h-3 w-24"></Skeleton>
+                        </div>
+                    ) : (
+                        <>
+                        <span>{profile?.name}</span>
+                        <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
+                        </>
+                    )}
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator></DropdownMenuSeparator>
-                <DropdownMenuItem>
-                    <Building className="mr-2 h-4 w-4"></Building>
-                    <span>Perfil da loja</span>
-                </DropdownMenuItem>
+                <DialogTrigger asChild>
+                    <DropdownMenuItem>
+                        <Building className="mr-2 h-4 w-4"></Building>
+                        <span>Perfil da loja</span>
+                    </DropdownMenuItem>
+                </DialogTrigger>
                 <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
                     <LogOut className="mr-2 h-4 w-4"></LogOut>
                     <span>Sair</span>
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
+        <StoreProfileDialog></StoreProfileDialog>
+        </Dialog>
     )
 }
